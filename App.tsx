ea4f544627c767f253ee61e14a77fa68c351a4bc
Tomasz <tomasz9531@gmail.com>
2022-10-09 13:08:38 +0200

    Setup navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>home screen</Text>
+        </View>
+    );
+}
 
 const App = () => {
     return (
-        <></>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-};
+}
 
 export default App;
